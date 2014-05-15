838afd86d4bdbac1c649184b399b8a4777085879
Stephane Vanacker <svanacker@talentsoft.com>
2014-05-15 21:06:03 +0200

    Kinect Test 1  : just initializing the Kinect Sensor in C# in WPF : introduce depth video + depth pixel count in a range

--- KinectTest2/KinectTest2/MainWindow.xaml.cs
+++ KinectTest2/KinectTest2/MainWindow.xaml.cs
@@ -45,8 +45,11 @@ namespace KinectTest2
                     kinect.Start();
                     KinectIdValue.Content = kinect.DeviceConnectionId;
                     LaunchButton.Content = "Stop";
-
                     kinect.ColorStream.Enable();
+                    kinect.ColorFrameReady += KinectOnColorFrameReady;
+                    kinect.DepthStream.Range = DepthRange.Default;
+                    kinect.DepthStream.Enable();
+                    kinect.DepthFrameReady += KinectOnDepthFrameReady;
                 }
                 else
                 {
@@ -58,23 +61,134 @@ namespace KinectTest2
             }
         }
 
+        private void KinectOnDepthFrameReady(object sender, DepthImageFrameReadyEventArgs depthImageFrameReadyEventArgs)
+        {
+            if (true == DepthRadioButton.IsChecked)
+            {
+                using (DepthImageFrame frame = depthImageFrameReadyEventArgs.OpenDepthImageFrame())
+                {
+                    ShowDepthImageFrame(frame);
+                }
+            }
+        }
+
+        private void KinectOnColorFrameReady(object sender, ColorImageFrameReadyEventArgs colorImageFrameReadyEventArgs)
+        {
+            if (true == VideoRadioButton.IsChecked)
+            {
+                using (ColorImageFrame frame = colorImageFrameReadyEventArgs.OpenColorImageFrame())
+                {
+                    ShowColorImageFrame(frame);
+                }
+            }
+        }
+
         private void TakePicture_Click(object sender, RoutedEventArgs e)
         {
-            using (var frame = kinect.ColorStream.OpenNextFrame(0))
+            using (ColorImageFrame frame = kinect.ColorStream.OpenNextFrame(0))
             {
-                var pixelData = new byte[frame.PixelDataLength];
-                frame.CopyPixelDataTo(pixelData);
-                int stride = frame.Width * frame.BytesPerPixel;
-                BitmapSource bitmapSource = BitmapSource.Create(frame.Width, frame.Height, 96, 96, PixelFormats.Bgr32, null, pixelData,
-                    stride);
-
-                ImageCanvas.Background = new ImageBrush(bitmapSource);
+                ShowColorImageFrame(frame);
             }
+        }
+
+        private void ShowDepthImageFrame(DepthImageFrame frame)
+        {
+            DepthImagePixel[] depthPixels = new DepthImagePixel[frame.PixelDataLength];
+            byte[] colorPixels = new byte[frame.PixelDataLength * sizeof(int)];
+            frame.CopyDepthImagePixelDataTo(depthPixels);
+
+            WriteableBitmap bitmap = new WriteableBitmap(frame.Width, frame.Height, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+            // Get the min and max reliable depth for the current frame
+            int minDepth = frame.MinDepth;
+            int maxDepth = frame.MaxDepth;
+
+            int depthInRangeCount = 0;
+
+            // Convert the depth to RGB
+            int colorPixelIndex = 0;
+            for (int i = 0; i < depthPixels.Length; ++i)
+            {
+                // Get the depth for this pixel
+                short depth = depthPixels[i].Depth;
+
+                if (depth > 400 && depth < 500)
+                {
+                    depthInRangeCount++;
+                }
+
+                // To convert to a byte, we're discarding the most-significant
+                // rather than least-significant bits.
+                // We're preserving detail, although the intensity will "wrap."
+                // Values outside the reliable depth range are mapped to 0 (black).
+
+                // Note: Using conditionals in this loop could degrade performance.
+                // Consider using a lookup table instead when writing production code.
+                // See the KinectDepthViewer class used by the KinectExplorer sample
+                // for a lookup table example.
+                byte intensity = (byte)(depth >= minDepth && depth <= maxDepth ? depth : 0);
+
+                // Write out blue byte
+                colorPixels[colorPixelIndex++] = intensity;
+
+                // Write out green byte
+                colorPixels[colorPixelIndex++] = intensity;
+
+                // Write out red byte                        
+                colorPixels[colorPixelIndex++] = intensity;
+
+                // We're outputting BGR, the last byte in the 32 bits is unused so skip it
+                // If we were outputting BGRA, we would write alpha here.
+                ++colorPixelIndex;
+            }
+
+            bitmap.WritePixels(new Int32Rect(0, 0, bitmap.PixelWidth, bitmap.PixelHeight), colorPixels, bitmap.PixelWidth * sizeof(int), 0);
+            ImageCanvas.Background = new ImageBrush(bitmap);
+
+            /*
+            frame.CopyPixelDataTo(pixelData);
+            int stride = frame.Width * frame.BytesPerPixel;
+            BitmapSource bitmapSource = BitmapSource.Create(frame.Width, frame.Height, 96, 96, PixelFormats.Bgr32, null, pixelData,
+                stride);
+
+            ImageCanvas.Background = new ImageBrush(bitmapSource);
+             * */
+
+            NearDepthCountValue.Content = depthInRangeCount.ToString();
+        }
+
+        private void ShowColorImageFrame(ColorImageFrame frame)
+        {
+            var pixelData = new byte[frame.PixelDataLength];
+            frame.CopyPixelDataTo(pixelData);
+            int stride = frame.Width * frame.BytesPerPixel;
+            BitmapSource bitmapSource = BitmapSource.Create(frame.Width, frame.Height, 96, 96, PixelFormats.Bgr32, null, pixelData,
+                stride);
+
+            ImageCanvas.Background = new ImageBrush(bitmapSource);
         }
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             kinect.ElevationAngle = (int) ElevationSlider.Value;
+        }
+
+        private void NearModeRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (kinect == null)
+            {
+                return;
+            }
+            kinect.DepthStream.Range = DepthRange.Near;
+        }
+
+        private void FarModeRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (kinect == null)
+            {
+                return;
+            }
+            kinect.DepthStream.Range = DepthRange.Default;
         }
     }
 }
