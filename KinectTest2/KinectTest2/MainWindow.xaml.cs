c2bd514b0dc68188f01ada40cc352b3c9d050f13
Mickaël Nivet <mnivet@talentsoft.com>
2015-01-26 19:05:24 +0100

    Cleanup some using and unused file

--- KinectTest2/KinectTest2/MainWindow.xaml.cs
+++ KinectTest2/KinectTest2/MainWindow.xaml.cs
@@ -1,23 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.Kinect;
-
-namespace KinectTest2
+﻿namespace KinectTest2
 {
+    using System;
     using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using Microsoft.Kinect;
+
     using Microsoft.Speech.AudioFormat;
     using Microsoft.Speech.Recognition;
 
