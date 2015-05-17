2bc2f63e2f5b1d9db1ef75373946fa17f5aa7394
AMontagu <adrienmontagu@gmail.com>
2015-05-17 19:00:33 +0200

    Create Lcd Management #2 : WriteLcdData with test

---
+++ RobotManager/Devices/Lcd/Com/LcdWriteData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.CodeDom;
+using System.Text;
+using Org.Cen.Communication.Utils;
+using Org.Cen.Com.Out;
+
+namespace Org.Cen.Devices.Lcd.Com
+{
+    public class LcdWriteData : OutData
+    {
+        public const string HEADER = "Lw";
+
+        public String Data;
+        public int Lenght;
+
+        public LcdWriteData(int lenght, string data)
+            : base()
+        {
+            if (data.Length > 4)
+            {
+                throw new ArgumentException("Parameter must be a string with four char", "original");
+            }
+            Data = data;
+            Lenght = lenght;
+        }
+
+        public override string getArguments()
+        {
+            //char array for convert message to write on the LCD
+            char[] character = this.Data.ToCharArray();
+            //String to return
+            StringBuilder result = new StringBuilder();
+            //Add the letters to write
+            for (int i = 0; i < Lenght; i++)
+            {
+                result.Append(DataParserUtils.format((int) character[i], 2));
+            }
+            return result.ToString();
+        }
+
+        public override string getHeader()
+        {
+            return HEADER;
+        }
+    }
+}
---
+++ RobotManager/DevicesTest/Lcd/LcdWriteDataTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using Org.Cen.Devices.Lcd.Com;
+
+namespace Org.Cen.Devices.Lcd
+{
+    using NUnit.Framework;
+    public class LcdWriteDataTest
+    {
+        [Test]
+        public void ShouldWriteLcdData4()
+        {
+            //Header = Lw
+            int lenght = 4;
+            String dataExpected = "defg";
+            //Result expected : Lw64656667 because d in hexa is 64 e is 65...
+
+            LcdWriteData writeData = new LcdWriteData(lenght, dataExpected);
+
+            string result = writeData.getMessage();
+            Debug.WriteLine(result);
+
+            Assert.AreEqual("Lw64656667", result);
+        }
+        public void ShouldWriteLcdData2()
+        {
+            //Header = Lw
+            int lenght = 2;
+            String dataExpected = "hi";
+            //Result expected : Lw6869
+
+            LcdWriteData writeData = new LcdWriteData(lenght, dataExpected);
+
+            string result = writeData.getMessage();
+
+            Assert.AreEqual("Lw6869", result);
+        }
+    }
+}
