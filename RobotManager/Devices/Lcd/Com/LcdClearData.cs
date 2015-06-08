fb6afa147d4815c9732bf5b7d9e824fea06f0185
Stephane Vanacker <svanacker@talentsoft.com>
2015-06-08 20:03:36 +0200

    Merge remote-tracking branch 'amontagu/AM3'

---
+++ RobotManager/Devices/Lcd/Com/LcdClearData.cs
@@ -0,0 +1,25 @@
+﻿namespace Org.Cen.Devices.Lcd.Com
+{
+    using Communication.Out;
+
+    public class LcdClearData : OutData
+    {
+        public const string HEADER = "Lc";
+
+        public LcdClearData()
+            : base()
+        {
+            
+        }
+
+        public override string GetArguments()
+        {
+            return ""; 
+        }
+
+        public override string GetHeader()
+        {
+            return HEADER;
+        }
+    }
+}
